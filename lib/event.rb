0aa5ca27943424529f4c39fc116cd5c18963c1eb
Brett Jones <bhjones4545@gmail.com>
2021-06-08 17:35:40 -0600

    Iteration 3 complete

--- lib/event.rb
+++ lib/event.rb
@@ -29,30 +29,30 @@ class Event
       craft.supplies_required
     end
 
-      array_hashes.flat_map do |hash|
-        hash.keys.map do |key|
-          key.to_s
-        end
-      end.uniq
-    end
-
-    def attendes_by_craft_interest
-      #need a hash of keys = craft and values that are arrays of names_array
-      c_interests = Hash.new { |hash, key| hash[key] = Array.new}
-      @crafts.each do |craft|
-        c_interests[craft.name]
-        @attendees.each do|person|
-          c_interests[craft.name] << person if person.interests.include?(craft.name)
-        end
+    array_hashes.flat_map do |hash|
+      hash.keys.map do |key|
+        key.to_s
       end
-      c_interests
-    end
+    end.uniq
+  end
 
-    def crafts_that_use(tool)
-      #find all crafts that use tool.  need to convert string to symbol
-      tool_sym = tool.to_sym
-      @crafts.find_all do |craft|
-        craft.supplies_required.keys.include?(tool_sym)
+  def attendes_by_craft_interest
+    #need a hash of keys = craft and values that are arrays of names_array
+    c_interests = Hash.new { |hash, key| hash[key] = Array.new}
+    @crafts.each do |craft|
+      c_interests[craft.name]
+      @attendees.each do|person|
+        c_interests[craft.name] << person if person.interests.include?(craft.name)
       end
+    end
+    c_interests
+  end
+
+  def crafts_that_use(tool)
+    #find all crafts that use tool.  need to convert string to symbol
+    tool_sym = tool.to_sym
+    @crafts.find_all do |craft|
+      craft.supplies_required.keys.include?(tool_sym)
     end
   end
+end
